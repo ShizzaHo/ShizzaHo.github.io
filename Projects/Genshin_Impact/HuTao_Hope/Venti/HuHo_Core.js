30babd436bfd6b85f90c5f16b195b560fb8fd377
ShizzaHo <74906456+ShizzaHo@users.noreply.github.com>
2021-03-06 20:37:17 +0500

    Update HuHo_Core.js

--- Projects/Genshin_Impact/HuTao_Hope/Venti/HuHo_Core.js
+++ Projects/Genshin_Impact/HuTao_Hope/Venti/HuHo_Core.js
@@ -1,7 +1,7 @@
 
 
 //  Обозначающие переменные
-var app_version = "1.0.2.1 - Verti Ver.";
+var app_version = "1.0.2.2 - Venti Version";
 var name_app = "HuTao Hope";
 var title_final = name_app +" "+ app_version;
 //  ---
