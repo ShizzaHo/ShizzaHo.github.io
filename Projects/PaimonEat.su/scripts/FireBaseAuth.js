8eb9b8fa2de8a54821b2b9ca74e3d10a5019e481
ShizzaHo <74906456+ShizzaHo@users.noreply.github.com>
2021-04-09 19:35:58 +0500

    Add files via upload

---
+++ Projects/PaimonEat.su/scripts/FireBaseAuth.js
@@ -0,0 +1,16 @@
+//Скрипт для подключения к базе данных
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAe8zxGLYHMcWeX0tSxrayZ7dQ6j1M5CVE",
+    authDomain: "dilucwine-f0daa.firebaseapp.com",
+    databaseURL: "https://dilucwine-f0daa-default-rtdb.firebaseio.com/",
+    projectId: "dilucwine-f0daa",
+    storageBucket: "dilucwine-f0daa.appspot.com",
+    messagingSenderId: "350341352955",
+    appId: "1:350341352955:web:c274d07887764787f3cf5e",
+    measurementId: "G-7ZH3QH6RMN"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
